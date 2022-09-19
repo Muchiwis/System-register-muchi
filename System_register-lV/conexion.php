7e8ee7fc6c27b909cf01395b0464e116360c89d5
Jhord Cristian <113742339+Muchiwis@users.noreply.github.com>
2022-09-19 11:18:37 -0500

    Add files via upload

--- System_register-lV/conexion.php
+++ System_register-lV/conexion.php
@@ -1,13 +1,15 @@
 <?php 
-try {
-    $enlace = new PDO('mysql:host=127.0.0.1;dbname=sistemaAcademico;','root','');
-    if(!empty($enlace)){
-        echo "Base de datos funcional";
-    }else{
-        echo "conexion fallida.".PHP_EOL;
-    }
-} catch (PDOException $th) {
-    echo "Error type: ".$th->getMessage();
-}
+ try {
+     $enlace = new PDO('mysql:host=127.0.0.1;dbname=sistema_registro;','root','');
+
+     if(!empty($enlace)){
+         //echo "Base de datos funcional";
+     }else{
+         echo "conexion fallida.".PHP_EOL;
+     }
+     $enlace->exec("SET CHARACTER SET utf8");
+ } catch (Exception $e) {
+        die('Error: '. $e->getMessage());
+ }
 
 ?>
--- System_register-lV/principal.php
+++ System_register-lV/principal.php
@@ -1,3 +1,14 @@
 <?php
-    echo "AQui se codificara todo lo que imcplica a la funcionalidad del sistmea";
+    require_once("conexion.php");
+    $sql="SELECT * FROM administradores";
+    $resultado=$enlace->prepare($sql);
+    //var_dump($resultado);
+    $resultado->execute(array());
+    
+      $cont=1;
+    while ($recorrido2=$resultado->fetch(PDO::FETCH_BOTH)){
+        //var_dump($recorrido2);
+        echo $cont.". Cod_ADM: ".$recorrido2[0]." Nombre: ".$recorrido2[1]." Apellido: ". $recorrido2[2]." Cargo: ".$recorrido2[3]. "<br>";
+        $cont++;
+    }
 ?>
